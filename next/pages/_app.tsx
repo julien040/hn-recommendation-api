727e564c18f272bd377bc4b0fcf2b4aa4d282fe2
julien040 <48369040+julien040@users.noreply.github.com>
2023-06-14 21:50:26 +0200

    ✨ Website all implemented

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,35 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       <Component {...pageProps} />
+      <footer className="py-4 text-slate-800 text-sm">
+        <p>
+          Made with ❤️ by{" "}
+          <Link
+            className="hover:underline"
+            href="https://github.com/julien040/"
+          >
+            Julien
+          </Link>
+        </p>
+
+        <p>
+          To contact me,
+          <Link className="hover:underline" href="https://julienc.me/whoami">
+            {" "}
+            check my website
+          </Link>
+        </p>
+        <br />
+        <p>
+          To learn more about the project, check the{" "}
+          <Link className="hover:underline" href="https://julienc.me/whoami">
+            blog post
+          </Link>
+        </p>
+        <p className="italic">
+          This is a personal project, not affiliated with Hacker News.
+        </p>
+      </footer>
     </>
   );
 }
